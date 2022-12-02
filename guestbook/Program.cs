0e828547aed8cb67ffc95c64b733422299305695
Lina Petersson <lipe2106@student.miun.se>
2022-12-02 13:31:58 +0100

    Felhantering och kommentarer

--- guestbook/Program.cs
+++ guestbook/Program.cs
@@ -2,7 +2,7 @@
     Simulate a guestbook.
     The datafile,'guestbook.json', created is in the format of Json.
 
-    Written by Lina Petersson / Mid Sweden University
+    Written by Lina Petersson / Student at Mid Sweden University
 */
 using System;
 using System.Collections.Generic;
@@ -19,29 +19,30 @@ namespace moment3
 
         public Guestbook()
         {
-            if (File.Exists(@"guestbook.json") == true)
+            if (File.Exists(filename) == true)
             { // If stored json data exists then read
                 string jsonString = File.ReadAllText(filename);
                 posts = JsonSerializer.Deserialize<List<Post>>(jsonString);
             }
         }
+
         public Post addPost(Post post)
         {
             posts.Add(post);
-            marshal();
+            marshal(); // Call class method marshal
             return post;
         }
 
         public int delPost(int index)
         {
-            posts.RemoveAt(index);
+            posts.RemoveAt(index); // Remove post at chosen index
             marshal();
             return index;
         }
 
         public List<Post> getPosts()
         {
-            return posts;
+            return posts; // Return all posts
         }
 
         private void marshal()
@@ -59,17 +60,17 @@ namespace moment3
         private string content;
         public string Author
         {
-            set { this.author = value; }
+            set { this.author = value; } // Set author input to class author
             get { return this.author; }
         }
         public string Title
         {
-            set { this.title = value; }
+            set { this.title = value; } // Set iput title to class title
             get { return this.title; }
         }
         public string Content
         {
-            set { this.content = value; }
+            set { this.content = value; } // Set input content to class content 
             get { return this.content; }
         }
     }
@@ -78,19 +79,27 @@ namespace moment3
     {
         static void Main(string[] args)
         {
-
+            // Create object of class Guestbook 
             Guestbook guestbook = new Guestbook();
             int i = 0;
 
             while (true)
             {
-                Console.Clear(); Console.CursorVisible = false;
-                Console.WriteLine("GÄSTBOK\n\n");
+                Console.Clear(); // Clear consol 
+                Console.CursorVisible = false; // Hide cursor
 
+                // Welcome user
+                Console.WriteLine("GÄSTBOK");
+                Console.WriteLine("Välkommen till gästboken!");
+                Console.WriteLine("\nMedan du är här kan du passa på att skriva ett nytt inlägg, ");
+                Console.WriteLine("eller radera ett befintligt som du inte är nöjd med. Mycket nöje!\n\n");
+
+                //Menu options
                 Console.WriteLine("1. Skriv ett nytt gästboksinlägg");
                 Console.WriteLine("2. Ta bort ett befintligt inlägg");
                 Console.WriteLine("X. Avsluta\n");
             
+                // Loop through posts and print 
                 i = 0;
                 foreach (Post post in guestbook.getPosts())
                 {
@@ -100,26 +109,35 @@ namespace moment3
                     Console.WriteLine("Innehåll: " + post.Content);
                 }
                
-                int inp = (int)Console.ReadKey(true).Key;
-                switch (inp)
+                int input = (int)Console.ReadKey(true).Key; // Save users menu choice 
+
+                // Depending on users menu choice show case
+                switch (input)
                 {
                     case '1':
                         Console.CursorVisible = true;
                         Post obj = new Post();
+
+                        // Ask user for input and save
                         Console.Write("\nAnge skribent: ");
-                        string author = Console.ReadLine();
+                        string author = Console.ReadLine(); 
                        
-                        while (author == "")
+                        // If input is empty print error message and ask for new input
+                        while (author == "") 
                         {
                             Console.Write("\nDu måste ange ett korrekt namn. Försök igen");
                             Console.Write("\nAnge skribent: ");
                             author = Console.ReadLine();
                         }
                         
+                        // When correct save input in Post object
                         obj.Author = author;
+
+                        // Ask user for input and save 
                         Console.Write("Ange titel: ");
                         string title = Console.ReadLine();
 
+                        // If input is empty print error message and ask for new input
                         while (title == "")
                         {
                             Console.Write("\nDu måste ange en korrekt titel. Försök igen");
@@ -127,10 +145,14 @@ namespace moment3
                             title = Console.ReadLine();
                         }
 
+                        // When correct save input in Post object
                         obj.Title = title;
-                      
+
+                        // Ask user for input and save 
                         Console.Write("Ange innehåll: ");
                         string content = Console.ReadLine();
+
+                        // If input is empty print error message and ask for new input
                         while (content == "")
                         {
                             Console.Write("\nDu måste ange ett korrekt innehåll. Försök igen");
@@ -138,17 +160,22 @@ namespace moment3
                             content = Console.ReadLine();
                         }
 
+                        // When correct save input in Post object
                         obj.Content = content;
-                        if (!String.IsNullOrEmpty(author) && !String.IsNullOrEmpty(title) && !String.IsNullOrEmpty(content)) guestbook.addPost(obj);
 
+                        // Add post to guestbook
+                        if (!String.IsNullOrEmpty(author) && !String.IsNullOrEmpty(title) && !String.IsNullOrEmpty(content)) guestbook.addPost(obj);
                         break;
                     case '2':
                         Console.CursorVisible = true;
                         Console.Write("\nAnge index att radera: ");
                         string index = Console.ReadLine();
+
+                        // Convert input to int and call method delPost in class Guestbook
                         guestbook.delPost(Convert.ToInt32(index));
                         break;
                     case 88:
+                        //Exit application
                         Environment.Exit(0);
                         break;
                 }
